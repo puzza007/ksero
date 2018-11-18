36a4610dad7bda73fa3f2ba0ebbae3e7d884e250
Paul Oliver <puzza007@gmail.com>
2018-11-19 11:56:34 +1300

    Parse args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+extern crate clap;
+use clap::App;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("Ksero")
+        .version("0.1.0")
+        .author("Paul Oliver <puzza007@gmail.com>")
+        .about("Duplicate File Finder")
+        .args_from_usage("--directories=<DIRECTORY>... 'Sets directories to search'")
+        .get_matches();
+
+    let directories: Vec<_> = matches.values_of("directories").unwrap().collect();
+    println!("Value for directory: {:?}", directories);
 }
